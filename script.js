d2656316938d0cd1c15d408fd75f904627fa1a63
Ritesh Mali <63728550+Rityamali@users.noreply.github.com>
2023-09-04 21:30:51 +0530

    Update script.js
    removerd js for nav

--- script.js
+++ script.js
@@ -9,23 +9,23 @@ document.addEventListener("mousemove",function(dets){
     crsr_b.style.top = dets.y-175+"px"
 })
 
-let navh4 = document.querySelectorAll("#nav h4")  //queryseectorAll is uesd to select all h4 in nav
-navh4.forEach(function(elem){
-    elem.addEventListener("mouseenter" , function(){
-        crsr.style.scale = 4
-        crsr.style.border = "1px solid white"
-        crsr.style.backgroundColor = "transparent"
-        elem.style.color = "black"
+// let navh4 = document.querySelectorAll("#nav h4")  //queryseectorAll is uesd to select all h4 in nav
+// navh4.forEach(function(elem){
+//     elem.addEventListener("mouseenter" , function(){
+//         crsr.style.scale = 4
+//         crsr.style.border = "1px solid white"
+//         crsr.style.backgroundColor = "transparent"
+//         elem.style.color = "black"
 
-    })
-    elem.addEventListener("mouseleave" , function(){
-        crsr.style.scale = 1
-        crsr.style.border = "0px solid orangered"
-        crsr.style.backgroundColor = "orangered"
-        elem.style.color = "white"
+//     })
+//     elem.addEventListener("mouseleave" , function(){
+//         crsr.style.scale = 1
+//         crsr.style.border = "0px solid orangered"
+//         crsr.style.backgroundColor = "orangered"
+//         elem.style.color = "white"
 
-    })
-})
+//     })
+// })
 
 
 
